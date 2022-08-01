f7f06f83ac5e118ab622bedcc25f1e19f45b675c
Alex Lucchesi <84862301+lucchesia7@users.noreply.github.com>
2022-08-01 13:32:49 -0700

    major update
    Major update to all files

---
+++ src/app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+from pathlib import Path
+import sys
+import os
+from classes.user_functions import User_Functions
+
+st.title("Magic the Gathering Card Viewer")
+
+card_name = st.text_input('Input card name:',value='Sol Ring')
+st.image(User_Functions().img_return(card_name.lower()))
+
+
+st.write(f"Here are 10 cards that would be recommended for your deck based off {card_name.title()}")
+st.image(User_Functions().recommended_cards(card_name = card_name))
