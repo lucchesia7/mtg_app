56fbea2ecc339259ceae6a25c0c1777880f4d33c
Alex Lucchesi <84862301+lucchesia7@users.noreply.github.com>
2022-11-22 13:33:24 -0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -6,18 +6,18 @@ user = User_Functions()
 
 
 def output():
-    try:
-        st.image(user.img_return(card_name))
-        img_list = user.recommended_cards(card_name=card_name)
-        st.write(
-            f"Here are 9 cards that would be recommended for your deck based off {card_name.title()}")
-        col1, col2, col3 = st.columns(3)
-        col1.image(img_list[0:3])
-        col2.image(img_list[4:7])
-        col3.image(img_list[8:11])
+    # try:
+    st.image(user.img_return(card_name))
+    img_list = user.recommended_cards(card_name=card_name)
+    st.write(
+        f"Here are 9 cards that would be recommended for your deck based off {card_name.title()}")
+    col1, col2, col3 = st.columns(3)
+    col1.image(img_list[0:3])
+    col2.image(img_list[4:7])
+    col3.image(img_list[8:11])
 
-    except BaseException:
-        st.error(f'{card_name.title()} is an invalid card. Please re-try with a valid card name. If the card name is valid, try typing the name exactly as it appears on the card.')
+    # except BaseException:
+    #     st.error(f'{card_name.title()} is an invalid card. Please re-try with a valid card name. If the card name is valid, try typing the name exactly as it appears on the card.')
 
 
 st.title("MTG Tracer")
