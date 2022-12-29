9fa783df0156790c1d9d2352866d935efec6f97c
Alex Lucchesi <84862301+lucchesia7@users.noreply.github.com>
2022-12-29 12:15:57 -0800

    Update recommender.py

--- src/pages/recommender.py
+++ src/pages/recommender.py
@@ -16,18 +16,18 @@ card_name = st.text_input(
     'Please Input the Full Name of the Card You Would Like to Find Recommendations for:'
     )
 if st.button('Submit Card'):
-    try:
-        st.image(user.img_return(card_name))
-        img_list = user.recommended_cards(card_name=card_name)
-        st.write(
+    # try:
+    st.image(user.img_return(card_name))
+    img_list = user.recommended_cards(card_name=card_name)
+    st.write(
             f"""Here are 9 cards that would be recommended for 
             your deck based off {card_name.title()}"""
             )
-        col1, col2, col3 = st.columns(3)
-        col1.image(img_list[0:3])
-        col2.image(img_list[4:7])
-        col3.image(img_list[8:11])
-    except BaseException:
-        st.error(f'''{card_name.title()} is an invalid card. 
-        Please re-try with a valid card name. If the card name is 
-        valid, try typing the name exactly as it appears on the card.''')
+    col1, col2, col3 = st.columns(3)
+    col1.image(img_list[0:3])
+    col2.image(img_list[4:7])
+    col3.image(img_list[8:11])
+    # except BaseException:
+        # st.error(f'''{card_name.title()} is an invalid card. 
+        # Please re-try with a valid card name. If the card name is 
+        # valid, try typing the name exactly as it appears on the card.''')
